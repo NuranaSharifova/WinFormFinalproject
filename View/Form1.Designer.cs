2cbaf71408cf405582e3c5fcb203c781279e455b
Sari_nu95@itstep.edu.az <Sari_nu95@itstep.edu.az>
2021-07-26 23:15:57 +0400

    demoedit

--- View/Form1.Designer.cs
+++ View/Form1.Designer.cs
@@ -36,18 +36,19 @@ namespace WinFormFinalproject
             this.Password = new System.Windows.Forms.Label();
             this.Loginbutton = new System.Windows.Forms.Button();
             this.Registerbutton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(50, 80);
+            this.textBox1.Location = new System.Drawing.Point(115, 209);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(313, 22);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(50, 148);
+            this.textBox2.Location = new System.Drawing.Point(115, 277);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(313, 22);
             this.textBox2.TabIndex = 1;
@@ -56,7 +57,7 @@ namespace WinFormFinalproject
             // 
             this.LoginName.AutoSize = true;
             this.LoginName.Font = new System.Drawing.Font("Modern No. 20", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LoginName.Location = new System.Drawing.Point(172, 47);
+            this.LoginName.Location = new System.Drawing.Point(237, 176);
             this.LoginName.Name = "LoginName";
             this.LoginName.Size = new System.Drawing.Size(109, 21);
             this.LoginName.TabIndex = 2;
@@ -66,7 +67,7 @@ namespace WinFormFinalproject
             // 
             this.Password.AutoSize = true;
             this.Password.Font = new System.Drawing.Font("Modern No. 20", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Password.Location = new System.Drawing.Point(172, 116);
+            this.Password.Location = new System.Drawing.Point(237, 245);
             this.Password.Name = "Password";
             this.Password.Size = new System.Drawing.Size(90, 21);
             this.Password.TabIndex = 3;
@@ -75,7 +76,7 @@ namespace WinFormFinalproject
             // Loginbutton
             // 
             this.Loginbutton.Font = new System.Drawing.Font("Modern No. 20", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Loginbutton.Location = new System.Drawing.Point(50, 205);
+            this.Loginbutton.Location = new System.Drawing.Point(115, 334);
             this.Loginbutton.Name = "Loginbutton";
             this.Loginbutton.Size = new System.Drawing.Size(124, 44);
             this.Loginbutton.TabIndex = 4;
@@ -86,7 +87,7 @@ namespace WinFormFinalproject
             // Registerbutton
             // 
             this.Registerbutton.Font = new System.Drawing.Font("Modern No. 20", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Registerbutton.Location = new System.Drawing.Point(241, 205);
+            this.Registerbutton.Location = new System.Drawing.Point(306, 334);
             this.Registerbutton.Name = "Registerbutton";
             this.Registerbutton.Size = new System.Drawing.Size(124, 44);
             this.Registerbutton.TabIndex = 5;
@@ -94,12 +95,23 @@ namespace WinFormFinalproject
             this.Registerbutton.UseVisualStyleBackColor = true;
             this.Registerbutton.Click += new System.EventHandler(this.Registerbutton_Click);
             // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(205, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 134);
+            this.button1.TabIndex = 6;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.ClientSize = new System.Drawing.Size(416, 312);
+            this.ClientSize = new System.Drawing.Size(567, 417);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.Registerbutton);
             this.Controls.Add(this.Loginbutton);
             this.Controls.Add(this.Password);
@@ -110,7 +122,6 @@ namespace WinFormFinalproject
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "StatusBook";
-         
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -124,6 +135,7 @@ namespace WinFormFinalproject
         private System.Windows.Forms.Label Password;
         private System.Windows.Forms.Button Loginbutton;
         private System.Windows.Forms.Button Registerbutton;
+        private System.Windows.Forms.Button button1;
     }
 }
 
